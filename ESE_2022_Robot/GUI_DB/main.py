686af0543981e898c89380c6854cd3fbcca84387
changhyune <ckdrn001@naver.com>
2022-05-22 23:12:27 +0900

    exercisecountsystemadd

--- ESE_2022_Robot/GUI_DB/main.py
+++ ESE_2022_Robot/GUI_DB/main.py
@@ -1,3 +1,4 @@
+from threading import Thread
 import time
 import sys
 from PyQt4 import QtGui, QtCore, uic
@@ -28,6 +29,7 @@ class UI(QtGui.QMainWindow, form_class):
         self.nowtime()
         self.exercount()
         self.th = Thread1()
+        self.th2 = Thread2()
     
     
     #button actions    
@@ -41,7 +43,8 @@ class UI(QtGui.QMainWindow, form_class):
         self.gotohome_3.clicked.connect(self.gotohome)
         self.gotohome_4.clicked.connect(self.gotohome)
         self.goto3_5.clicked.connect(self.goto3)
-        self.startfinish_2.clicked.connect(self.start_finish)
+        #self.startfinish_2.clicked.connect(self.start_finish)
+        self.startfinish_2.clicked.connect(self.workoutestimate)
         
         self.giverecord_3.clicked.connect(self.giverecord)
         self.givefeedback_3.clicked.connect(self.givefeedback)
@@ -68,6 +71,7 @@ class UI(QtGui.QMainWindow, form_class):
             nowday = "day"+str(i)+"_3"
             getattr(self, nowday).setText(current_day.addDays(i-7).toString('MM.dd'))
 
+        # weekrecord
         for i in range(7):
             nowday = current_day.addDays(-i).toString('yyyy-MM-dd')
             row = mydb.returnDayworkoutRows(nowday)
@@ -230,6 +234,24 @@ class UI(QtGui.QMainWindow, form_class):
         mydb = database.db()
         #mydb.insertWorkoutData()
         print(self.routine_1.item(0, 0).text())
+        
+    def workoutestimate(self):
+        global a
+        a = a+1
+        self.th2.start() 
+        self.th2.exername.connect(self.label_7.setText)
+        self.th2.exerset.connect(self.leftset_2.setText)
+        self.th2.exercount.connect(self.lefttry_2.setText)
+        self.th2.resttime.connect(self.resttime_2.setText)
+        #
+        #woondongsizak
+        # if a%2 == 0:
+        #     self.th2.estimateworkout(b,c,d,e)
+        #     self.th2.exername.connect(self.label_7.setText)
+        #     self.th2.exerset.connect(self.leftset_2.setText)
+        #     self.th2.exercount.connect(self.lefttry_2.setText)
+        #     self.th2.resttime.connect(self.resttime_2.setText)
+               
             
         
 class Thread1(QThread): 
@@ -249,8 +271,35 @@ class Thread1(QThread):
             self.change_value1.emit(str(i)) 
             time.sleep(2) 
         self.change_value2.emit("arrive.")
+        
+class Thread2(QThread):
+    
+    exername = pyqtSignal(str)
+    exerset = pyqtSignal(str)
+    exercount = pyqtSignal(str)
+    resttime = pyqtSignal(str)
+    def __init__(self): 
+       QThread.__init__(self)       
 
-            
+    def run(self):
+        name = "benchpress"
+        set = 5
+        count = 10
+        rest = 20
+        self.exername.emit(name)
+        for i in range(set):
+            self.exerset.emit(str(set))
+            for j in range(count):
+                self.exercount.emit(str(count))
+                count =count-1
+                #print("?")
+                time.sleep(1)
+            set = set-1
+            for k in range(rest):
+                self.resttime.emit(str(rest-k))
+                time.sleep(1)
+        
+               
     
             
         
